1259f81f2d4006be71d7b82897d8ba7b91e38cc6
reina <62531225+reinana@users.noreply.github.com>
2021-12-04 11:12:00 +0900

    ArrayListに初期化子でadd

--- Sample.java
+++ Sample.java
@@ -19,16 +19,24 @@ public class Sample {
 
         // new Outer("hoge").new Inner().execute();
 
-        List<Item> list = new ArrayList<>();
-        list.add(new Item("apple",100));
-        list.add(new Item("banana",80));
-        list.add(new Item("orange",120));
+        // List<Item> list = new ArrayList<>();
+        // list.add(new Item("apple",100));
+        // list.add(new Item("banana",80));
+        // list.add(new Item("orange",120));
 
         // Collections.sort(list); Itemクラスに並び替えメソッドがあるときComparableを実装してないといけない
         // Collections.sort(list, new Item.ItemNameComparator()); // 引数2個ノトキ、Comparatorを実装したクラスを引数に入れる
-        Collections.sort(list, Item.getPriceComparator());
-        for(Item item: list) {
-            System.out.println(item);
+        // Collections.sort(list, Item.getPriceComparator());
+        // for(Item item: list) {
+        //     System.out.println(item);
+        // }
+
+        List<String> list = new ArrayList<>() {
+            {//初期化ブロック コンストラクタより先に実行
+                super.add("A");
+                super.add("B");
+                super.add("C");
+            }
         }
 
     }
