78ef3cc88ef608b9e696418f55c1a31ca36fdcde
reina <62531225+reinana@users.noreply.github.com>
2021-12-05 22:56:27 +0900

    gitHubから編集できる

--- Sample.java
+++ Sample.java
@@ -5,6 +5,7 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
+// gitHubから編集できる
 
 
 public class Sample {
